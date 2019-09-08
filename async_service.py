0eb2dc99e2d430fc7f7e8dd35196ecfcb5e1b5ac
= <=>
2019-09-08 23:27:21 +0300

    Update requirements. First route for price prediction

--- async_service.py
+++ async_service.py
@@ -1,4 +1,27 @@
 import os
+import ast
+import pandas as pd
 from aiohttp import web
 
 FOLDER_WITH_MODELS = '../apartmentML/models/'
+
+
+application_routes = web.RouteTableDef()
+
+
+@application_routes.get('/predictPrice')
+async def predict_price(request):
+    data = await request.json()
+    data = ast.literal_eval(data)
+    data_to_predict = pd.DataFrame([data])
+    print(data_to_predict)
+
+    return web.Response(text='Hello')
+
+
+if __name__ == '__main__':
+
+    app = web.Application()
+    app.add_routes(application_routes)
+    web.run_app(app, host='localhost', port=3030)
+
